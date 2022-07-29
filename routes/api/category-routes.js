b406961ef89222c167e6ad4a9c891a99f7816080
Moetive <Moetive206@gmail.com>
2022-07-29 05:26:25 -0700

    Update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -22,22 +22,64 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findByPk();
+  try {
+    const db_catData = await Category.findByPk(req.params.id, {
+      attributes: ["id", "category_name"],
+      include: [
+        {
+          model: Product,
+          attributes: ["category_id"],
+        },
+      ],
+    });
+    res.status(200).json(db_catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const db_catData = await Category.create(req.body);
+    res.status(200).json(db_catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const db_catData = await Category.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json(db_catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const db_catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!db_catData) {
+      res.status(404).json({ message: "No Category Found With This ID" });
+      return;
+    }
+    res.status(200).json(db_catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
